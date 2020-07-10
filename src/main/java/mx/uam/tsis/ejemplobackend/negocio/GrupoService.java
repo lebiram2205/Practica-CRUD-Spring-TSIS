b616b736d85120a91e3aa9b7fbd09ef79bb68644
lebiram2205 <maribelcontrerasvidal@yahoo.com.mx>
2020-07-09 21:14:32 -0500

    Pruena de logeo

--- src/main/java/mx/uam/tsis/ejemplobackend/negocio/GrupoService.java
+++ src/main/java/mx/uam/tsis/ejemplobackend/negocio/GrupoService.java
@@ -17,46 +17,46 @@ public class GrupoService {
 	private GrupoRepository gruporepository;
 	@Autowired
 	private AlumnoService alumnoservice;
-	
+
 	public Grupo create(Grupo nuevo) {
-		//en dado caso validad reglas de negocio si es que existe
+		// en dado caso validad reglas de negocio si es que existe
 		return gruporepository.save(nuevo);
-		
-		
+
 	}
-	public Iterable <Grupo> retriveAll(){
+
+	public Iterable<Grupo> retriveAll() {
 		return gruporepository.findAll();
 	}
-	//agregar un alumno al grupo
-	//metodo que conecte los objetos
-	//son dos llaves primarias
+
+	// agregar un alumno al grupo
+	// metodo que conecte los objetos
+	// son dos llaves primarias
 	public boolean addStudentToGrup(Integer groupId, Integer matricula) {
-		log.info("Agregando alumno con matricula"+matricula+"grupo" + groupId);
-		//1.-Recuperamos primero al alumno
-		Alumno alumno=alumnoservice.retrieve(matricula);
-		
-		//2.-Recuperamos el grupo
-		Optional <Grupo> grupoOp= gruporepository.findById(groupId);
-		
-		//si grupo o matricula no estan presentes o no se encuentran
-		//3.-Verificamos que el grupo y el alumno exitan
-		if(!grupoOp.isPresent()|| alumno==null) {
-			
+		log.info("Agregando alumno con matricula" + matricula + "grupo" + groupId);
+		// 1.-Recuperamos primero al alumno
+		Alumno alumno = alumnoservice.retrieve(matricula);
+
+		// 2.-Recuperamos el grupo
+		Optional<Grupo> grupoOp = gruporepository.findById(groupId);
+
+		// si grupo o matricula no estan presentes o no se encuentran
+		// 3.-Verificamos que el grupo y el alumno exitan
+		if (!grupoOp.isPresent() || alumno == null) {
+
 			return false;
-			
+
 		}
-		//conectar el grupo con el alumno
-		//4.-Agrego el alumno al grupo
-		Grupo  grupo =grupoOp.get();
+		// conectar el grupo con el alumno
+		// 4.-Agrego el alumno al grupo
+		Grupo grupo = grupoOp.get();
 		grupo.addAlumno(alumno);
-		
-		
-		//hay que persistir ek cambio
-		//5.-Persistir el cambio
+
+		// hay que persistir ek cambio
+		// 5.-Persistir el cambio
 		gruporepository.save(grupo);
 		return true;
 	}
-	
+
 	public Grupo update(Integer id, Grupo grupoActualizar) {
 		Optional<Grupo> grupo = gruporepository.findById(id);
 		if (!grupo.isPresent()) {
@@ -78,9 +78,5 @@ public class GrupoService {
 		Optional<Grupo> grupo = gruporepository.findById(id);
 		return grupo.isPresent();
 	}
-
-	
-	
-	
 
 }
