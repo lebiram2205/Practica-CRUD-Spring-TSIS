b694c7503db65e792996919cca4ad9e4cd91668b
lebiram2205 <maribelcontrerasvidal@yahoo.com.mx>
2020-07-09 21:22:33 -0500

    Prueba de logueo

--- src/test/java/mx/uam/tsis/ejemplobackend/servicios/AlumnoControllerIntregationTest.java
+++ src/test/java/mx/uam/tsis/ejemplobackend/servicios/AlumnoControllerIntregationTest.java
@@ -20,53 +20,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Optional;
+
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AlumnoControllerIntregationTest {
-	
+
 	@Autowired
 	private TestRestTemplate restTemplate;
-	
+
 	@Autowired
 	private AlumnoRepository alumnoRepository;
-	
+
 	@BeforeEach
 	public void prepare() {
-		
+
 		// Aqui se puede hacer cosas para preparar sus casos de prueba, incluyendo
 		// agregar datos a la BD
-		
+
 	}
-	
-	
+
 	@Test
 	public void testCreate201() {
-		
-		
+
 		Alumno alumno = new Alumno();
 		alumno.setCarrera("Computación");
 		alumno.setMatricula(12345678);
 		alumno.setNombre("Pruebin");
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("content-type",MediaType.APPLICATION_JSON.toString());
-		
-		HttpEntity <Alumno> request = new HttpEntity <> (alumno, headers);
-		
-		ResponseEntity <Alumno> responseEntity = restTemplate.exchange("/alumnos", HttpMethod.POST, request, Alumno.class);
+		headers.set("content-type", MediaType.APPLICATION_JSON.toString());
 
-		log.info("Me regresó:"+responseEntity.getBody());
-		
+		HttpEntity<Alumno> request = new HttpEntity<>(alumno, headers);
+
+		ResponseEntity<Alumno> responseEntity = restTemplate.exchange("/alumnos", HttpMethod.POST, request,
+				Alumno.class);
+
+		log.info("Me regresó:" + responseEntity.getBody());
+
 		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-		
-		Optional <Alumno> optAlumno = alumnoRepository.findById(12345678);
-		assertEquals(alumno,optAlumno.get());
-		
+
+		Optional<Alumno> optAlumno = alumnoRepository.findById(12345678);
+		assertEquals(alumno, optAlumno.get());
+
 	}
 
 	@Test
 	public void testCreate400() {
-		
+
 	}
 
 }
